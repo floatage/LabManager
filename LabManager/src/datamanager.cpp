b0c0360298ddda632836a39278a6b75e10bd6864
floatage <1300730251@qq.com>
2017-02-26 23:12:51 +0800

    完善数据管理部分

--- LabManager/src/datamanager.cpp
+++ LabManager/src/datamanager.cpp
@@ -1,7 +1,124 @@
 #include "datamanager.h"
 
-DataManager::DataManager()
+DataManager* DataManager::instance = nullptr;
+DataManager::DataManager(QObject* parent):QObject(parent)
 {
 
 }
 
+DataManager::~DataManager()
+{
+
+}
+
+DataManager* DataManager::getInstance()
+{
+    if (instance == nullptr){
+        instance = new DataManager();
+    }
+
+    return instance;
+}
+
+const DataManager::DataProvider* getDataProvider(const QString& uri)
+{
+    std::pair<multimap<QString,DataProvider>::iterator, multimap<QString,int>::DataProvider> it = providerMap.equal_range(uri);
+    if (it.first != it.second){
+        return *(it.first);
+    }
+
+    return nullptr;
+}
+
+bool DataManager::registerDataProvider(const DataProvider& provider)
+{
+    providerMap[provider.getMarkString() = provider.clone();
+    return true;
+}
+
+bool DataManager::unRegisterDataProvider(const DataProvider& provider)
+{
+    std::pair<multimap<QString,DataProvider>::iterator, multimap<QString,int>::DataProvider> it = providerMap.equal_range(provider.getMarkString());
+
+    for (std::multimap<QString, DataProvider>::iterator begin = it.first; begin != it.second; ++begin){
+        if (begin->getStorageWay() == provider.getStorageWay()){
+            providerMap.erase(begin);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool DataManager::add(const QString& uri, const StorableObject& data)
+{
+    return false;
+}
+
+bool DataManager::remove(const QString& uri, const QString& selection, const std::vector<QString>& selectionArgs)
+{
+    return false;
+}
+
+bool DataManager::update(const QString& uri, const StorableObject& data, const QString& selection, const std::vector<QString>& selectionArgs)
+{
+    return false;
+}
+
+bool DataManager::query(const QString& uri, const QString& condition, const QString& selection, const std::vector<QString>& selectionArgs, QString order)
+{
+    return false;
+}
+
+
+//UserDataProvider implement
+
+UserDataProvider::UserDataProvider()
+{
+
+}
+
+UserDataProvider::~UserDataProvider()
+{
+
+}
+
+bool UserDataProvider::onCreate()
+{
+    return false;
+}
+
+bool UserDataProvider::add(const StorableObject& data)
+{
+    return false;
+}
+
+bool UserDataProvider::remove(const QString& selection, const std::vector<QString>& selectionArgs)
+{
+    return false;
+}
+
+bool UserDataProvider::update(const StorableObject& data, const QString& selection, const std::vector<QString>& selectionArgs)
+{
+    return false;
+}
+
+bool UserDataProvider::query(const QString& condition, const QString& selection, const std::vector<QString>& selectionArgs, QString order)
+{
+    return false;
+}
+
+QString UserDataProvider::getMarkString()const
+{
+    return "/labmanager/user";
+}
+
+QString UserDataProvider::getStorageWay()const
+{
+    return "database";
+}
+
+const DataProvider& clone()const
+{
+    return DataProvider(*this);
+}
--- LabManager/src/datamanager.h
+++ LabManager/src/datamanager.h
@@ -11,14 +11,27 @@ class DataManager: public QObject
 {
     Q_OBJECT
 public:
-    DataManager(QObject* parent);
-    const DataProvider* getDataProvider();
+    static DataManager* getInstance();
+
+    bool registerDataProvider(const DataProvider& provider);
+    bool unRegisterDataProvider(const DataProvider& provider);
+
+    bool add(const QString& uri, const StorableObject& data);
+    bool remove(const QString& uri, const QString& selection, const std::vector<QString>& selectionArgs);
+    bool update(const QString& uri, const StorableObject& data, const QString& selection, const std::vector<QString>& selectionArgs);
+    bool query(const QString& uri, const QString& condition, const QString& selection, const std::vector<QString>& selectionArgs, QString order);
 
 signals:
 
 public slots:
 
 private:
+    DataManager(QObject* parent);
+    ~DataManager();
+    const DataProvider* getDataProvider(const QString& uri);
+
+    static DataManager* instance;
+
     std::multimap<QString, DataProvider> providerMap;
 };
 
@@ -30,19 +43,32 @@ public:
     bool update(const StorableObject& data, const QString& selection, const std::vector<QString>& selectionArgs)=0;
     bool query(const QString& condition, const QString& selection, const std::vector<QString>& selectionArgs, QString order)=0;
     QString getMarkString()const=0;
+    QString getStorageWay()const=0;
+    const DataManager& clone()const=0;
+
+    bool operator <(const DataProvider& other){return getMarkString() == other.getMarkString() && getStorageWay() == other.getStorageWay();}
 
 protected:
     bool onCreate()=0;
+    virtual ~DataProvider(){}
+    DataProvider(){}
 };
 
-
-class DataProviderProxy
+class UserDataProvider: public DataProvider
 {
 public:
-    bool add(const QString& uri, const StorableObject& data);
-    bool remove(const QString& uri, const QString& selection, const std::vector<QString>& selectionArgs);
-    bool update(const QString& uri, const StorableObject& data, const QString& selection, const std::vector<QString>& selectionArgs);
-    bool query(const QString& uri, const QString& condition, const QString& selection, const std::vector<QString>& selectionArgs, QString order);
+    bool add(const StorableObject& data);
+    bool remove(const QString& selection, const std::vector<QString>& selectionArgs);
+    bool update(const StorableObject& data, const QString& selection, const std::vector<QString>& selectionArgs);
+    bool query(const QString& condition, const QString& selection, const std::vector<QString>& selectionArgs, QString order);
+    QString getMarkString()const;
+    QString getStorageWay()const;
+    const DataManager& clone()const;
+
+protected:
+    bool onCreate();
+    virtual ~UserDataProvider();
+    UserDataProvider();
 };
 
 #endif // DATAMANAGER_H
